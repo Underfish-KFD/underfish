160767fcf85e3a1a3ba1574829846ef3d354b572
VanHellthing <vanhellthing@mail.ru>
2025-12-07 13:57:27 +0300

    added repository

---
+++ src/main/kotlin/ru/underfish/app/database/dao/CommunityMemberRepository.kt
@@ -0,0 +1,7 @@
+package ru.underfish.app.database.dao
+
+import ru.underfish.app.database.entities.CommunityMember
+
+interface CommunityMemberRepository : AbstractRepository<CommunityMember> {
+
+}
---
+++ src/main/kotlin/ru/underfish/app/database/dao/EventRepository.kt
@@ -0,0 +1,6 @@
+package ru.underfish.app.database.dao
+
+
+import ru.underfish.app.database.entities.Event
+
+interface EventRepository : AbstractRepository<Event> {}
