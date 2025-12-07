668b9987d29ca0633548494b88a2257884bf80ec
VanHellthing <vanhellthing@mail.ru>
2025-12-07 15:19:46 +0300

    update profiles setting, add SecurityConfig

---
+++ src/main/kotlin/ru/underfish/app/config/SecurityConfig.kt
@@ -0,0 +1,50 @@
+package ru.underfish.app.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    @Profile("!test")
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            csrf {
+                disable()
+            }
+            authorizeHttpRequests {
+                authorize("/api/v1/users/register", permitAll)
+                authorize("/api/v1/users/login", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Profile("test")
+    fun testSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize(anyRequest, permitAll)
+            }
+            csrf { disable() }
+            headers {
+                frameOptions { disable() }
+            }
+        }
+        return http.build()
+    }
+}
